a871b599637de3c72fcc98fb214a9abf8f3e1dee
evertonbrunosds <evertonbrunosds@gmail.com>
2020-06-30 19:41:26 -0300

    SDSFinance

---
+++ src/view/ControlCenter.java
@@ -0,0 +1,54 @@
+/*
+ * This file is part of the SDSFinance Open Source Project.
+ * SDSFinance is licensed under the GNU GPLv3.
+ *
+ * Copyright (c) 2020. Everton Bruno Silva dos Santos <evertonbrunogithub@yahoo.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package view;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+/**
+ * Classe responsável por comportar-se como centro de controle da interface.
+ * @author Everton Bruno Silva dos Santos.
+ */
+public abstract class ControlCenter {
+    
+    /**
+     * Método responsável por alinhar as linhas e colunas de uma tabela.
+     * @param table Refere-se a tabela.
+     * @param align Refere-se ao alinhamento.
+     */
+    public static void alignTo(final JTable table, final int align) {
+        ((DefaultTableCellRenderer) table.getTableHeader().getDefaultRenderer()).setHorizontalAlignment(align);
+        for(int i = 0; i < table.getColumnCount(); i++) {
+            table.getColumnModel().getColumn(i).setCellRenderer(cellRenderer(align));
+        }
+    }
+    
+    /**
+     * Método responsável por criar e retornar um modelo de coluna alinhada.
+     * @param alignment Refere-se ao alinhamento.
+     * @return Retorna modelo de coluna alinhada.
+     */
+    private static DefaultTableCellRenderer cellRenderer(final int alignment) {
+        final DefaultTableCellRenderer align = new DefaultTableCellRenderer();
+        align.setHorizontalAlignment(alignment);
+        return align;
+    }
+    
+}
